35271292d7117c1d840629b9709c5a91e00fcf03
denys-bobrovnykov <62986552+denys-bobrovnykov@users.noreply.github.com>
2020-11-20 13:31:17 +0200

    Hash router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import { HashRouter, Route, Switch, Link } from 'react-router-dom';
 import { fetchData } from '../../redux/actions/actionCreators';
 import { Employees } from '../Employees/Employees';
 import { EmployeesBirthday } from '../EmployeesBirthday/EmployeesBirthday';
@@ -12,7 +12,7 @@ function App() {
     dispatch(fetchData());
   }, []);
   return (
-    <Router>
+    <HashRouter>
       <Switch>
         <Route path="/test-task" exact>
           <Link id="employees-link" to="/test-task/employees">
@@ -26,7 +26,7 @@ function App() {
           </main>
         </Route>
       </Switch>
-    </Router>
+    </HashRouter>
   );
 }
 
